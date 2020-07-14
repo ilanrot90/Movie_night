4dbd981349c53d07eae9c3b4098e3f53f4e32ac7
ilanrot <ilanrot90@gmail.com>
2020-07-14 17:38:34 +0300

    add color to theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -4,6 +4,7 @@ import { centerFlex } from "./sharedStyle";
 declare module "styled-components" {
   export interface DefaultTheme {
     light: string;
+    background: string;
     main: string;
     dark: string;
     contrastText: string;
@@ -15,19 +16,21 @@ declare module "styled-components" {
 
 const theme = {
   light: {
-    light: "#e7cbeb",
-    main: "#e1bee7",
-    dark: "#9d85a1",
-    contrastText: "#000",
+    background: "#f9fafa",
+    light: "#757ce8",
+    main: "#3f50b5",
+    dark: "#002884",
+    contrastText: "#222831",
     utils: {
       centerFlex,
     },
   },
   dark: {
-    light: "#757ce8",
-    main: "#3f50b5",
-    dark: "#002884",
-    contrastText: "#fff",
+    background: "#222831",
+    light: "#e7cbeb",
+    main: "#e1bee7",
+    dark: "#9d85a1",
+    contrastText: "#f9fafa",
     utils: {
       centerFlex,
     },
