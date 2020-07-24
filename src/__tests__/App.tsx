f82bc5916be8db387089cefa26a0420fd01ef423
ilanrot <ilanrot90@gmail.com>
2020-07-24 15:55:36 +0300

    first test pass

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -2,7 +2,7 @@ import { renderUi, waitFor, screen } from 'testingUtils/utils';
 
 describe('Render app', () => {
 	test('test', async () => {
-		renderUi({ route: '/login' });
-		expect(screen.getAllByAltText('login')).toBeInTheDocument();
+		renderUi({ route: '/app' });
+		expect(screen.getByRole('button', { name: /log out/i })).toBeInTheDocument();
 	});
 });
