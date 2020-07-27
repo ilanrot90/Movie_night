5059499215c9069e9f5d06ac9e1fd037dbedff05
ilanrot <ilanrot90@gmail.com>
2020-07-27 20:52:36 +0300

    add title for navbar

--- src/components/AppLayout/MenuAppBar.tsx
+++ src/components/AppLayout/MenuAppBar.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import styled from 'styled-components';
 import { debounce } from 'utils/lodash.utils';
 import SearchInput from 'components/common-ui/SearchInput';
+import { Text } from 'style/sharedStyle';
 
-const TopNav = styled.div`
+const TopNav = styled.div<{ isOnTop: boolean }>`
 	max-width: 100%;
 	height: 60px;
 	position: sticky;
 	display: flex;
-	background-color: ${({ theme }) => theme.dark};
+	justify-content: space-between;
+	align-items: center;
+	background-color: ${({ theme, isOnTop }) => (isOnTop ? 'transparent' : theme.dark)};
+	transition: background-color 0.125s ease-in;
 	padding: ${({ theme }) => theme.spacing.m}px;
 `;
 
 const Input = styled.div`
-	margin: auto 0 auto auto;
+	margin: auto 0;
+`;
+
+const Title = styled(Text)`
+	margin-left: ${({ theme }) => theme.spacing.xxl * 4}px;
 `;
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -30,14 +38,27 @@ const useStyles = makeStyles((theme: Theme) =>
 	})
 );
 
+type Listener = EventListenerOrEventListenerObject;
+
 export default function MenuAppBar() {
 	const classes = useStyles();
+	const [isOnTop, setPositionTop] = useState<boolean>(true);
+	useEffect(() => {
+		const handleScroll: Listener = e => {
+			const scrollToList = !(document.body.scrollTop > 50);
+			setPositionTop(scrollToList);
+		};
+		document.addEventListener('scroll', handleScroll);
+		return () => {
+			document.removeEventListener('mousedown', handleScroll);
+		};
+	});
 	const handleChange = (value: string) => {
 		console.log({ value });
 	};
 
 	return (
-		<TopNav>
+		<TopNav isOnTop={!isOnTop}>
 			<IconButton
 				className={classes.menuButton}
 				aria-label="account of current user"
@@ -47,6 +68,9 @@ export default function MenuAppBar() {
 			>
 				<Avatar>IR</Avatar>
 			</IconButton>
+			<Title as="h1" size={22}>
+				Title
+			</Title>
 			<Input>
 				<SearchInput callBack={debounce(handleChange, 500)} />
 			</Input>
