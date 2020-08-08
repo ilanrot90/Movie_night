d73e56707bc44b784a644836af34ace88f886183
ilanrot <ilanrot90@gmail.com>
2020-08-08 11:49:15 +0300

    add header component

--- src/screens/MoviesScreen/MoviesLibrary.tsx
+++ src/screens/MoviesScreen/MoviesLibrary.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 const Movies = () => {
-	return <div>movies</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 };
 
 export default React.memo(Movies);
---
+++ src/screens/MoviesScreen/components/Header.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const Header = () => {
+	const [,] = useState();
+
+	useEffect(() => {}, []);
+	return <div>header</div>;
+};
+
+export default Header;
