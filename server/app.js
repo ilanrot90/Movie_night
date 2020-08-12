c119fced61f65690cc729404e8c44edbcfe47a41
ilanrot <ilanrot90@gmail.com>
2020-08-12 12:33:22 +0300

    add heroku config + server

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('*', function (req, res) {
+	res.sendFile(path(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT);
