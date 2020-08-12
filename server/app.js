4bfbadfaddddddd2406e694e81fae367068a1e12
ilanrot <ilanrot90@gmail.com>
2020-08-12 13:45:33 +0300

    fix path to favicon and static files

--- server/app.js
+++ server/app.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(favicon(__dirname + '../build/favicon.ico'));
+app.use(favicon(__dirname + '/../build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '/../build')));
 
 app.get('/*', function (req, res) {
-	res.sendFile(path(__dirname, '../build/index.html'));
+	res.sendFile(path(__dirname, '/../build/index.html'));
 });
 
 app.listen(PORT);
