735e3e195c688d290e4988a813f5b2fb3069c2b7
ilanrot <ilanrot90@gmail.com>
2020-08-12 13:33:26 +0300

    add favicon support

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('*', function (req, res) {
+app.use(favicon(__dirname + '../build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', function (req, res) {
 	res.sendFile(path(__dirname, '../build/index.html'));
 });
 
