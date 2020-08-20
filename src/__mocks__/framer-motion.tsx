2317070d9bf8328a81a6ffff4b2ce89e133f3b47
ilanrot <ilanrot90@gmail.com>
2020-08-20 17:56:17 +0300

    add requireActual methods to 'framer-motion'

--- src/__mocks__/framer-motion.tsx
+++ src/__mocks__/framer-motion.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react';
 
 jest.mock('framer-motion', () => ({
+	...jest.requireActual<{}>('framer-motion'),
 	motion: {
 		path: () => <path />,
 		svg: () => <svg />,
 		div: ({ children, ...rest }: { children: ReactElement; 'data-testid': string }) => <div {...rest}>{children}</div>,
 	},
-	AnimatePresence: ({ children }: { children: ReactElement }) => <div>{children}</div>,
 	useMotionValue: jest.fn().mockReturnValue(0),
 	useTransform: jest.fn().mockReturnValue(1),
 }));
