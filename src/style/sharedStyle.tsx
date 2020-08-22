ad522f07104b72be5f3a04bdbbcce4ff861f5231
ilanrot <ilanrot90@gmail.com>
2020-08-23 01:10:11 +0300

    update snapshots to match new style

--- src/style/sharedStyle.tsx
+++ src/style/sharedStyle.tsx
@@ -144,10 +144,14 @@ export const scrollbar = css`
 export const HEADER_HEIGHT = '76px';
 export const GridContainer = styled.div<{ minColumnSize?: number; rowHeight?: number }>`
 	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(${({ minColumnSize = 200 }) => minColumnSize}px, 1fr));
-	grid-auto-rows: ${({ rowHeight = 320 }) => rowHeight}px;
+	grid-template-columns: repeat(auto-fit, minmax(${({ minColumnSize = 240 }) => minColumnSize}px, 1fr));
+	grid-auto-rows: ${({ rowHeight = 480 }) => rowHeight}px;
 	grid-column-gap: ${({ theme }) => theme.spacing.xl}px;
 	grid-row-gap: ${({ theme }) => theme.spacing.xxl}px;
+	@media ${device.tablet} {
+		grid-template-columns: repeat(auto-fit, minmax(${({ minColumnSize = 200 }) => minColumnSize}px, 1fr));
+		grid-auto-rows: ${({ rowHeight = 320 }) => rowHeight}px;
+	}
 `;
 export const GridItem = styled.div`
 	position: relative;
