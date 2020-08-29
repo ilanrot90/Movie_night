d2bcee8feeee8f36848011adf41c516c9db99c3c
ilanrot <ilanrot90@gmail.com>
2020-08-29 17:35:29 +0300

    add HexToRgba util to theme

--- src/utils/strings.utils.ts
+++ src/utils/strings.utils.ts
@@ -1,2 +1,3 @@
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const nameRegex = /^[a-zA-Z\s]*$/;
+export const extendHex = (v: string): string => v.replace(/#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])/, '#$1$1$2$2$3$3');
